080f0bb64b55efcba76ad25332347bb007138421
awjans <andreww@jans.org>
2019-12-21 15:28:16 -1000

    reworking web directories

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+apt -y update
+apt -y upgrade
+apt -y install git
 mkdir /src
 git clone https://github.com/awjans/jans.git /src/jans
 chmod ug+x /src/jans/awslightsail.cfg.sh /src/jans/refresh.sh
--- refresh.sh
+++ refresh.sh
@@ -4,6 +4,6 @@ pwd
 git pull
 find -type d -exec chmod -R 755 {} \;
 find -type f -exec chmod -R 644 {} \;
-chown -R root:www-data php/www
-chmod 755 refresh.sh
+chown -R root:www-data php
+chmod 750 refresh.sh
 cp apache2/jans.org.conf /etc/apache2/sites-available/jans.org.conf
